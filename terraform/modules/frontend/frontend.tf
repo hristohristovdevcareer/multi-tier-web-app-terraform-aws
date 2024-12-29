caec6761b57cde25da835488e2762a183664b2c5
@hristo.hristov.dev.career <hristo.hristov.dev.career@gmail.com>
2024-12-29 00:10:40 +0000

    fixed a typo

--- terraform/modules/frontend/frontend.tf
+++ terraform/modules/frontend/frontend.tf
@@ -14,60 +14,60 @@ resource "aws_launch_template" "frontend" {
   }
 
   user_data = base64encode(<<EOT
-    #!/bin/bash
-    echo ECS_CLUSTER=${var.ECS_CLUSTER} >> /etc/ecs/ecs.config
+      #!/bin/bash
+      echo ECS_CLUSTER=${var.ECS_CLUSTER} >> /etc/ecs/ecs.config
 
-    LOG_FILE="/var/log/user_data.log"
-    exec > >(tee -a $LOG_FILE) 2>&1
+      LOG_FILE="/var/log/user_data.log"
+      exec > >(tee -a $LOG_FILE) 2>&1
 
-    # Update and install Docker
-    apt-get update -y && apt-get install -y docker.io || { echo "Docker installation failed"; exit 1; }
-    sudo apt install aws-cli --classic
+      # Update and install Docker
+      apt-get update -y && apt-get install -y docker.io || { echo "Docker installation failed"; exit 1; }
+      sudo apt install aws-cli --classic
 
-    systemctl start docker
-    systemctl enable docker
-    usermod -aG docker ubuntu
+      systemctl start docker
+      systemctl enable docker
+      usermod -aG docker ubuntu
 
-    sudo docker run --name ecs-agent \
-    --detach=true \
-    --restart=always \
-    --network=host \
-    --env=ECS_CLUSTER=${var.ECS_CLUSTER} \
-    --volume=/var/run/docker.sock:/var/run/docker.sock \
-    --volume=/var/log/ecs/:/log \
-    --volume=/var/lib/ecs/data:/data \
-    --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
-    amazon/amazon-ecs-agent:latest
+      sudo docker run --name ecs-agent \
+      --detach=true \
+      --restart=always \
+      --network=host \
+      --env=ECS_CLUSTER=${var.ECS_CLUSTER} \
+      --volume=/var/run/docker.sock:/var/run/docker.sock \
+      --volume=/var/log/ecs/:/log \
+      --volume=/var/lib/ecs/data:/data \
+      --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro \
+      amazon/amazon-ecs-agent:latest
 
-    # Set up SSH directory and permissions
-    mkdir -p /home/ubuntu/.ssh
-    chmod 700 /home/ubuntu/.ssh
+      # Set up SSH directory and permissions
+      mkdir -p /home/ubuntu/.ssh
+      chmod 700 /home/ubuntu/.ssh
 
-    # Create known_hosts file
-    touch /home/ubuntu/.ssh/known_hosts
-    chmod 644 /home/ubuntu/.ssh/known_hosts
-    echo "Created known_hosts"
+      # Create known_hosts file
+      touch /home/ubuntu/.ssh/known_hosts
+      chmod 644 /home/ubuntu/.ssh/known_hosts
+      echo "Created known_hosts"
 
-    # Ensure gitlab.com is a known host
-    ssh-keyscan -t rsa gitlab.com >> /home/ubuntu/.ssh/known_hosts
-    echo "Gitlab host ssh scanned and added to known_hosts"
+      # Ensure gitlab.com is a known host
+      ssh-keyscan -t rsa gitlab.com >> /home/ubuntu/.ssh/known_hosts
+      echo "Gitlab host ssh scanned and added to known_hosts"
 
-    # Set up SSH keys
-    echo "${var.GITLAB_PRIVATE_KEY}" | base64 --decode > /home/ubuntu/.ssh/gitlab_rsa
-    chmod 400 /home/ubuntu/.ssh/gitlab_rsa
-    echo "${var.GITLAB_PUBLIC_KEY}" > /home/ubuntu/.ssh/gitlab_rsa.pub
-    echo "SSH keys set up"
+      # Set up SSH keys
+      echo "${var.GITLAB_PRIVATE_KEY}" | base64 --decode > /home/ubuntu/.ssh/gitlab_rsa
+      chmod 400 /home/ubuntu/.ssh/gitlab_rsa
+      echo "${var.GITLAB_PUBLIC_KEY}" > /home/ubuntu/.ssh/gitlab_rsa.pub
+      echo "SSH keys set up"
 
-    # Log in to ECR
-    aws ecr get-login-password --region ${var.REGION} | \
-    docker login --username AWS --password-stdin ${var.FE_ECR_REPO} || { echo "ECR login failed"; exit 1; }
+      # Log in to ECR
+      aws ecr get-login-password --region ${var.REGION} | \
+      docker login --username AWS --password-stdin ${var.FE_ECR_REPO} || { echo "ECR login failed"; exit 1; }
 
-    # Pull the Docker image
-    docker pull ${var.FE_ECR_REPO}:latest || { echo "Docker pull failed"; exit 1; }
+      # Pull the Docker image
+      docker pull ${var.FE_ECR_REPO}:latest || { echo "Docker pull failed"; exit 1; }
 
-    # Run the Docker container
-    docker run -d -p 3000:3000 -e ${var.FE_ECR_REPO}:latest || { echo "Docker run failed"; exit 1; }
-    EOT
+      # Run the Docker container
+      docker run -d -p 3000:3000 ${var.FE_ECR_REPO}:latest || { echo "Docker run failed"; exit 1; }
+      EOT
   )
 
   network_interfaces {
