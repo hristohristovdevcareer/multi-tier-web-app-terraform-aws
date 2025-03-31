181fe5f31b96153eb3e0533bbaa727e87df2782c
@hristo.hristov.dev.career <hristo.hristov.dev.career@gmail.com>
2025-03-31 20:57:14 +0100

    testing pipeline

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <section className="flex flex-col py-20">
       <Header title="Hello Everyone!" description="This is a great first devops project!" />
-      <div className="w-full max-w-96 mx-auto my-5">
+      <div className="w-full max-w-[1024px] mx-auto mb-5">
         <hr></hr>
       </div>
       <Content />
